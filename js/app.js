c3872508271a50f818025b897b012a64b5b93cf7
ellisorane <ellisorane@yahoo.com>
2021-02-01 15:18:55 -0500

    First commit

--- js/app.js
+++ js/app.js
@@ -24,12 +24,43 @@ $(document).ready(function () {
     }
     navSlide()
 
-    $.ajax({url: "https://mockend.com/org/drag-knee-motorcycles/", success: function(result){
-        console.log(result)
-        // $('.catsDiv').html(() => {
-        //     result.motorcycles.forEach(data => {
-        //         `<p>${data.make}</p>`
-        //     })
-        // })
+    //ADDS COMMAS TO NUMBERS >= 1000
+    function formatNumber(num) {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
+
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: "./js/data.json",
+        success: function (result) {
+            console.log(result)
+            result.forEach(data => {
+                $('.catsDiv').append(`<button class="catsBtn" id="catsMake">${data.make} +</button>`)
+                $('.inventory').append(`
+                <div class="item">
+                    <a href="#">
+                        <div class="imgArea" style="background-image: url(${data.imgs[0]});">
+                            <div class="priceDiv">
+                                <p class="price">$${formatNumber(data.price)} <span class="imgsLength">📷${data.imgs.length}</span></p>
+                            </div>
+                        </div>
+                        <div class="itemInfoContainer">
+                            <div class="itemInfo">
+                                <div class="itemName"><strong>${data.year} ${data.make} ${data.model} 250</strong></div>
+                                <div class="condition">${data.condition}</div>
+                                <div class="mileage">Mileage: ${data.mileage} miles</div>
+                                <div class="color">Color: ${data.color}</div>
+                            </div>
+                            <div class="itemBtnContainer">
+                                <button class="itemBtn">✉ Email</button> <button class="itemBtn">☎ Call</button> 
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            `)
+                // $('.catsDiv').append(`<button class="catsBtn" id="catsMake">${data.model} +</button>`)
+                // $('.catsDiv').append(`<button class="catsBtn" id="catsMake">${data.condition} +</button>`)
+            })
     }})
 })
